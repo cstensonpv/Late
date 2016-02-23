0f00ed356ddad4184fc9ef70b327dc787f2437fb
Arvid Sätterkvist <arvidsat@gmail.com>
2016-02-23 10:17:19 +0100

    Removed node_modules

--- server/httprequest.js
+++ server/httprequest.js
@@ -1,23 +1,93 @@
-var request = require("request");
+// var request = require("request");
+var SL = require('sl-api');
+ 
+var sl = new SL({
+  realtimeInformation: "a759eafc14934dd681cb2f542e99330b",
+  locationLookup: "fb98d1b0f1f841b2bfe48b43e48f77e4"
+}, 'json');
 
+
+// Realtime data
 (function() {
-	var data;
+	var data = [];
+	var totalDelays = [];
 
-	module.exports.requestRealTimeData = function() {
-		request({
-			uri: "http://api.sl.se/api2/realtimedepartures.json?key=a759eafc14934dd681cb2f542e99330b&siteid=9192&timewindow=5",
-			method: "GET",
-			timeout: 5000,
-			followRedirect: true,
-			maxRedirects: 10
-		}, function(error, response, body) {
-			data = body;
-			console.log("realtimedata updated");
-		});
+	module.exports.requestRealTimeData = function(siteids) {
+		for (var i = 0; i < siteids.length; i++) {
+			sl.realtimeInformation({siteid: siteids[i], timewindow: 20}, function(err, results) {
+				if(err) {
+					console.log("Error: " + err);
+				}
+				if (results) {
+					var d = JSON.parse(results);
+					var trains = d.ResponseData.Trains;
+					data.push(trains);
+					var timetable;
+					var expected;
+					var delay = 0;
+					var siteid = trains[0].SiteId;
+					// console.log(trains[0].SiteId);
+					if(trains.length > 0) {
+						for (var j = 0; j < trains.length; j++) {
+							timetable = new Date(trains[j].TimeTabledDateTime);
+							expected = new Date(trains[j].ExpectedDateTime);
+							var tempDelay = expected.getTime() - timetable.getTime(); 
+							if (tempDelay > 0) {
+								delay += tempDelay;
+							} 
+						}
+						console.log(siteid + " " + delay);
+						totalDelays.push({
+							siteid: siteid,
+							delayMS: delay
+						});
+					} else {
+						console.log(siteid + " has no departues");
+					}
+				}
+			});
+		}
 	};
 
-	module.exports.getRealTimeData = function() {
-		return data;
+	module.exports.getDelayData = function(siteid) {
+		for (var i = 0; i < totalDelays.length; i++) {
+			if (totalDelays[i].siteid == siteid) {
+				return totalDelays[i].delayMS;
+			}
+		}
+		return "No data found";
+	};
+
+	module.exports.getRealTimeData = function(siteid) {
+		for (var i = 0; i < data.length; i++) {
+			if (data[i][0].SiteId == siteid) {
+				return data[i];
+			}
+		}
+		return "No data found";
+	};
+}());
+
+// Location data
+(function() {
+	var siteid;
+
+	module.exports.requestLocationData = function(search) {
+		console.log(search);
+		sl.locationLookup({searchstring: search})
+		.then(function(data) {
+			data = JSON.parse(data);
+			// console.log(data.ResponseData);
+			siteid = data.ResponseData[0].SiteId;
+			console.log("location data updated");
+			// console.log(siteid);
+		})
+		.fail(console.error)
+		.done();
+	};
+
+	module.exports.getLocationData = function() {
+		return siteid;
 	};
 }());
 
--- server/main.js
+++ server/main.js
@@ -5,11 +5,80 @@ var app = express();
 
 var fetchInterval = 10000;
 
-app.get('/realtimedata', function(req, res) {
-	res.send(httprequest.getRealTimeData());
+// var siteids = [
+// 	// Stockholm C
+// 	{
+// 		siteid: 9000,
+// 		connectedStations: [
+// 			9510
+// 		]
+// 	},
+// 	// Karlberg C
+// 	{
+// 		siteid: 9510,
+// 		connectedStations: [
+// 			9000,
+// 			9509
+// 		]
+// 	},
+// 	// Solna C
+// 	{
+// 		siteid: 9509,
+// 		connectedStations: [
+// 			9000,
+// 			9509
+// 		]
+// 	},
+
+// ];
+
+var siteids = [
+	9000, // Stockholm C
+	9510, // Karlberg
+	9509, // Solna
+	9508, // Ulriksdal
+	9507, // Helenelund
+	9506, // Sollentuna
+	9505, // häggvik
+	9504, // Norrviken
+	9503, // Rotebro
+];
+
+
+
+// var train = {
+// 	lastStation: 9000,
+// 	nextStation: 9510,
+// 	leftMinAgo: 2,
+// 	arriveMin: 3
+// };
+
+app.get('/test', function(req, res) {
+	res.send("Hello");
 });
 
-httprequest.requestRealTimeData();
+app.get('/realtimedata', function(req, res) {
+	if (req.query.siteid) {
+		res.sendStatus(httprequest.getRealTimeData(req.query.siteid));
+	} else {
+		res.sendStatus("No siteid");
+	}
+});
+
+app.get('/locationdata', function(req, res) {
+	res.sendStatus(httprequest.getLocationData(res.query.siteid));
+});
+
+app.get('/delaydata', function(req, res) {
+	if (req.query.siteid) {
+		res.sendStatus(httprequest.getDelayData(req.query.siteid));
+	} else {
+		res.sendStatus("No siteid");
+	}
+});
+
+httprequest.requestRealTimeData(siteids);
+// httprequest.requestLocationData("Rotebro");
 
 /////////////////// Uncomment when ready /////////////////
 // setInterval(function() {
