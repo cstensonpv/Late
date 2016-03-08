fb412137135613f848c0e34f6e80f720f7436c04
Arvid Sätterkvist <arvidsat@gmail.com>
2016-03-08 14:07:02 +0100

    Push so I can continue working on another computer

--- server/httprequest.js
+++ server/httprequest.js
@@ -1,9 +1,12 @@
 // var request = require("request");
 var SL = require('sl-api');
+var fs = require('fs');
+var siteids = require('./siteids');
  
 var sl = new SL({
   realtimeInformation: "a759eafc14934dd681cb2f542e99330b",
-  locationLookup: "fb98d1b0f1f841b2bfe48b43e48f77e4"
+  locationLookup: "fb98d1b0f1f841b2bfe48b43e48f77e4",
+  tripPlanner: "4b570251388b4e33afeb139f007952b7"
 }, 'json');
 
 
@@ -11,6 +14,16 @@ var sl = new SL({
 (function() {
 	var data = [];
 	var totalDelays = [];
+	var currentMinute = 0;
+	var lineids = [];
+
+	// Load the data
+	console.log("loading data...");
+	for (var i = 0; i < siteids.length; i++) {
+		data['id_' + siteids[i]] = JSON.parse(fs.readFileSync('./data/' + siteids[i] + '.json', 'utf8'));
+	}
+	lineids['J35'] = JSON.parse(fs.readFileSync('./json/J35.json', 'utf8'));
+	console.log("done.");
 
 	function addRealtimeData(d) {
 		for (var i = 0; i < data.length; i++) {
@@ -33,47 +46,39 @@ var sl = new SL({
 		totalDelays.push(d);
 	}
 
-	module.exports.requestRealTimeData = function(siteid) {
-		sl.realtimeInformation({siteid: siteid, timewindow: 20}, function(err, results) {
-			if(err) {
-				console.log("Error: " + err);
-			}
-			if (results) {
-				var d = JSON.parse(results);
-				var trains = d.ResponseData.Trains;
-				addRealtimeData(trains);
-				var timetable;
-				var expected;
-				var delayNorth = 0;
-				var delaySouth = 0;
-				var siteid = trains[0].SiteId;
-				// console.log(trains[0].SiteId);
-				if(trains.length > 0) {
-					for (var j = 0; j < trains.length; j++) {
-						timetable = new Date(trains[j].TimeTabledDateTime);
-						expected = new Date(trains[j].ExpectedDateTime);
-						var tempDelay = expected.getTime() - timetable.getTime(); 
-						if (tempDelay > 0) {
-							if (trains[j].JourneyDirection == 1) {
-								delaySouth += tempDelay;
-							} else if (trains[j].JourneyDirection == 2){
-								delayNorth += tempDelay;
-							} else {
-								console.log("Journey direction missmatch");
-							}
-						}
-					}
-					console.log(siteid + " " + delayNorth + " " + delaySouth);
-					addDelayData({
-						siteid: siteid,
-						delayNorth: delayNorth,
-						delaySouth: delaySouth
-					});
-				} else {
-					console.log(siteid + " has no departues");
+	function getRealTimeData(siteid) {
+		var d = data['id_' + siteid];
+		if (d !== undefined) {
+			for (var i = 0; i < d.length; i++) {
+				if (d[i].time === currentMinute) {
+					return d[i].data;
 				}
 			}
-		});
+		}
+		return "No data found";
+	}
+
+	module.exports.requestRealTimeData = function(siteid) {
+	};
+
+	module.exports.getDelaysFrom = function(siteid) {
+		var d = getRealTimeData(siteid);
+		var lineNumbers = ["35", "36", "37"];
+		var lines = [];
+
+		for (var i = 0; i < lineNumbers.length; i++) {
+			lines['J' + lineNumbers[i]] = [];
+		}
+
+		for (var i = 0; i < d.length; i++) {
+			if (lineNumbers.indexOf(d[i].LineNumber) > -1) {
+				lines['J' + d[i].LineNumber].push(d[i]);
+			}
+		}
+
+		for (var i = 0; i < lineNumbers.length; i++) {
+
+		}
 	};
 
 	module.exports.getDelayDataSiteid = function(siteid) {
@@ -91,12 +96,11 @@ var sl = new SL({
 	};
 
 	module.exports.getRealTimeData = function(siteid) {
-		for (var i = 0; i < data.length; i++) {
-			if (data[i][0].SiteId == siteid) {
-				return data[i];
-			}
-		}
-		return "No data found";
+		return getRealTimeData(siteid);
+	};
+
+	module.exports.setCurrentMinute = function(n) {
+		currentMinute = n;
 	};
 }());
 
@@ -120,6 +124,14 @@ var sl = new SL({
 
 	module.exports.getLocationData = function() {
 		return siteid;
+	};
+}());
+
+(function() {
+	module.exports.requestTrip = function(originId, destId) {
+		sl.tripPlanner.trip({originId: originId, destId: destId}, function(err, results) {
+			console.log(results);
+		});
 	};
 }());
 
--- server/main.js
+++ server/main.js
@@ -3,52 +3,9 @@ var express = require('express');
 var httprequest = require('./httprequest');
 var app = express();
 
-var fetchInterval = 4000;
-
-// var siteids = [
-// 	// Stockholm C
-// 	{
-// 		siteid: 9000,
-// 		connectedStations: [
-// 			9510
-// 		]
-// 	},
-// 	// Karlberg C
-// 	{
-// 		siteid: 9510,
-// 		connectedStations: [
-// 			9000,
-// 			9509
-// 		]
-// 	},
-// 	// Solna C
-// 	{
-// 		siteid: 9509,
-// 		connectedStations: [
-// 			9000,
-// 			9509
-// 		]
-// 	},
+var fetchInterval = 500;
 
 // ];
-
-var siteids = [
-	9000, // Stockholm C
-	9510, // Karlberg
-	// 9509, // Solna
-	// 9508, // Ulriksdal
-	// 9507, // Helenelund
-	// 9506, // Sollentuna
-	// 9505, // häggvik
-	// 9504, // Norrviken
-	// 9503, // Rotebro
-	// 9502, // Upplands väsby
-	// 9501, // Rosersberg
-	// 9500, // Märsta
-	// 9511, // Arlanda
-	// 6086, // Uppsala
-];
-
 app.get('/test', function(req, res) {
 	res.send("Hello");
 });
@@ -73,43 +30,25 @@ app.get('/delaydata', function(req, res) {
 	}
 });
 
-// httprequest.requestLocationData("uppsala");
+app.get('/delaydatafrom', function(req, res) {
+	if (req.query.siteid) {
+		res.jsonp(httprequest.getDelaysFrom(req.query.siteid));
+	} else {
+		res.jsonp("No siteid");
+	}
+});
 
-///////////////////// Test purposes /////////////////////
-// Fetch the data only once.
-for (var i = 0; i < siteids.length; i++) {
-	httprequest.requestRealTimeData(siteids[i]);
-}
+var cm = -1;
+var setMinute = function() {
+	cm++;
+	if (cm >= 35) {
+		cm = 0;
+	}
+	// console.log("Current minute: " + cm);
+	httprequest.setCurrentMinute(cm);
+};
 
-/////////////////// Uncomment when ready /////////////////
-// siteidCounter = 0;
-// setInterval(function() {
-// 	console.log(siteids[siteidCounter]);
-// 	httprequest.requestRealTimeData(siteids[siteidCounter]);
-// 	siteidCounter++;
-// 	if (siteidCounter >= siteids.length) {
-// 		siteidCounter = 0;
-// 	}
-// }, fetchInterval);
-
-// Add headers
-// app.use(function (req, res, next) {
-
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
-
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET');
-
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     // res.setHeader('Access-Controsl-Allow-Credentials', true);
-
-//     // Pass to next layer of middleware
-//     next();
-// });
+setMinute();
+setInterval(setMinute, fetchInterval);
 
 var server = app.listen(3000);
